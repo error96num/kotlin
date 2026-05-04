43865085689c227ed4d8db91ebae7a66483b7dc5
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2026-05-04 10:46:45 +0200

    [K/N] Deserialize C-interop Klibs to IR directly from metadata, w/o K1
    This commit refactors the deserialization of C-interop Klibs in the
    second stage of compilation from "Metadata -> Descriptors -> Lazy IR"
    pipeline to direct "Metadata -> IR" conversion. Although desciptors are
    still used from time to time (mainly module and package descriptors),
    this significantly reduces the reliance on K1 and particularly psi2ir
    from the K/N backend.
    
    While IrProviderForCEnumAndCStructStubs was not strictly a part of
    deserialization process, it also had to be rewritten. Previously it
    looked up declarations via descriptors, then looked up an IR element
    for a given descriptor. Now it, both can and has to, search the IR
    directly. This also simplified the implementation quite a lot, to the
    point it now fits well into a single class.
    
    The biggest side effect is that the resulting IR for interop Klibs is
    now non-lazy. That should simplify the compliation process, particularly
    because this was one of the two remaining places where lazy IR was
    created in the K/N backend. The other place - forward declated classes -
    will be migrated soon as well, so lazy IR will be no more.
    But this also results in all class members being deserialized, even when
    that could not be necessary, such as when a class is only used as a type.
    However, if it ever becomes a performance problem, it should be quite
    easy to fix by only attepting to deserialize actually referenced (or
    otherwise useful) class members.
    
    #KT-83940 Fixed

--- compiler/ir/serialization.native/src/org/jetbrains/kotlin/backend/konan/serialization/KonanIrLinker.kt
+++ compiler/ir/serialization.native/src/org/jetbrains/kotlin/backend/konan/serialization/KonanIrLinker.kt
@@ -105,6 +105,7 @@ class KonanIrLinker(
             cInteropModuleDeserializerFactory.createIrModuleDeserializer(
                 moduleDescriptor,
                 klib,
+                this,
             )
         }
         else -> {
--- kotlin-native/backend.native/compiler/ir/backend.native/build.gradle.kts
+++ kotlin-native/backend.native/compiler/ir/backend.native/build.gradle.kts
@@ -40,6 +40,8 @@ dependencies {
     implementation(project(":native:binary-options"))
     implementation(project(":compiler:cli:cli-native-klib"))
     implementation(project(":native:native.config"))
+    implementation(project(":kotlinx-metadata-klib"))
+    compileOnly(project(":kotlin-metadata")) // Only to fix IDE reporting unresolved references.
 
     testImplementation(kotlinTest("junit"))
 }
