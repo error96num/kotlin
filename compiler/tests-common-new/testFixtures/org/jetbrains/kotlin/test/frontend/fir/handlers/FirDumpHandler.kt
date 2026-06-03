1fc313542b3528d7e37257b78f9b0dbdbb04228c
Brian Norman <brian.norman@jetbrains.com>
2026-06-03 15:17:46 -0500

    [Test] Introduce DUMP_CLASSIFIER directive for dump files
    WIP

--- compiler/tests-common-new/testFixtures/org/jetbrains/kotlin/test/frontend/fir/handlers/FirDumpHandler.kt
+++ compiler/tests-common-new/testFixtures/org/jetbrains/kotlin/test/frontend/fir/handlers/FirDumpHandler.kt
@@ -14,15 +14,8 @@ import org.jetbrains.kotlin.fir.declarations.FirResolvePhase
 import org.jetbrains.kotlin.fir.extensions.generatedMembers
 import org.jetbrains.kotlin.fir.extensions.generatedNestedClassifiers
 import org.jetbrains.kotlin.fir.moduleData
-import org.jetbrains.kotlin.fir.renderer.FirClassMemberRenderer
-import org.jetbrains.kotlin.fir.renderer.FirDeclarationRenderer
-import org.jetbrains.kotlin.fir.renderer.FirDeclarationRendererWithFilteredAttributes
-import org.jetbrains.kotlin.fir.renderer.FirPackageDirectiveRenderer
-import org.jetbrains.kotlin.fir.renderer.FirRenderer
-import org.jetbrains.kotlin.fir.renderer.FirSymbolRendererWithStaticFlag
+import org.jetbrains.kotlin.fir.renderer.*
 import org.jetbrains.kotlin.fir.symbols.lazyDeclarationResolver
-import org.jetbrains.kotlin.test.TestInfrastructureInternals
-import org.jetbrains.kotlin.test.backend.handlers.assertFileDoesntExist
 import org.jetbrains.kotlin.test.directives.CodegenTestDirectives.CHECK_BYTECODE_LISTING
 import org.jetbrains.kotlin.test.directives.ConfigurationDirectives.DISABLE_TYPEALIAS_EXPANSION
 import org.jetbrains.kotlin.test.directives.FirDiagnosticsDirectives
@@ -30,16 +23,17 @@ import org.jetbrains.kotlin.test.directives.FirDiagnosticsDirectives.DISABLE_FIR
 import org.jetbrains.kotlin.test.directives.FirDiagnosticsDirectives.EXPLICITLY_GENERATE_PLUGIN_FILES
 import org.jetbrains.kotlin.test.directives.FirDiagnosticsDirectives.RENDER_FIR_DECLARATION_ATTRIBUTES
 import org.jetbrains.kotlin.test.directives.FirDiagnosticsDirectives.USE_LATEST_LANGUAGE_VERSION
+import org.jetbrains.kotlin.test.directives.assertEqualsToDump
+import org.jetbrains.kotlin.test.directives.getClassifiedDumpFile
+import org.jetbrains.kotlin.test.directives.getDefaultDumpFile
 import org.jetbrains.kotlin.test.directives.model.DirectivesContainer
 import org.jetbrains.kotlin.test.directives.model.RegisteredDirectives
 import org.jetbrains.kotlin.test.frontend.fir.FirOutputArtifact
 import org.jetbrains.kotlin.test.frontend.fir.FirOutputPartForDependsOnModule
-import org.jetbrains.kotlin.test.impl.testConfiguration
 import org.jetbrains.kotlin.test.model.TestModule
 import org.jetbrains.kotlin.test.services.TestServices
 import org.jetbrains.kotlin.test.services.moduleStructure
 import org.jetbrains.kotlin.test.utils.MultiModuleInfoDumper
-import java.io.File
 
 @OptIn(DirectDeclarationsAccess::class)
 class FirDumpHandler(
@@ -56,8 +50,9 @@ class FirDumpHandler(
         for (part in info.partsForDependsOnModules) {
             val currentModule = part.module
             byteCodeListingEnabled = byteCodeListingEnabled || CHECK_BYTECODE_LISTING in module.directives
-            val isFirDumpEnabled =
-                expectedFile().exists() || FirDiagnosticsDirectives.FIR_DUMP in currentModule.directives
+            val isFirDumpEnabled = FirDiagnosticsDirectives.FIR_DUMP in currentModule.directives ||
+                    testServices.moduleStructure.getDefaultDumpFile(getDumpExtension()).exists() ||
+                    testServices.moduleStructure.getClassifiedDumpFile(getDumpExtension()).exists()
 
             if (!isFirDumpEnabled) return
 
@@ -82,31 +77,11 @@ class FirDumpHandler(
 
     override fun processAfterAllModules(someAssertionWasFailed: Boolean) {
         if (testServices.moduleStructure.allDirectives.shouldSkip()) return
-        val expectedFile = expectedFile()
-
-        if (dumper.isEmpty()) {
-            assertions.assertFileDoesntExist(expectedFile, FirDiagnosticsDirectives.FIR_DUMP)
-        } else {
-            val actualText = dumper.generateResultingDump()
-            assertions.assertEqualsToFile(expectedFile, actualText, message = { "Content is not equal" })
-        }
+        val actualText = if (dumper.isEmpty()) null else dumper.generateResultingDump()
+        assertEqualsToDump(getDumpExtension(), actualText)
     }
 
-    private fun expectedFile(): File {
-        // TODO: change according to multiple testdata files
-        val testDataFile = testServices.moduleStructure.originalTestDataFiles.first()
-        val extension = if (byteCodeListingEnabled) ".fir2.txt" else ".fir.txt"
-        val originalExpectedFilePath = testDataFile.parentFile.resolve("${testDataFile.nameWithoutFirExtension}$extension").path
-
-        @OptIn(TestInfrastructureInternals::class)
-        val expectedFilePath = testServices.testConfiguration
-            .metaTestConfigurators
-            .fold(originalExpectedFilePath) { fileName, configurator ->
-                configurator.transformTestDataPath(fileName)
-            }
-
-        return File(expectedFilePath)
-    }
+    private fun getDumpExtension(): String = if (byteCodeListingEnabled) ".fir2.txt" else ".fir.txt"
 
     private class FirClassMemberRendererWithGeneratedDeclarations(val session: FirSession) : FirClassMemberRenderer() {
         override fun render(regularClass: FirRegularClass) {
