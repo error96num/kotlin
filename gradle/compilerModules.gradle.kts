121fd6e2b0b88962a6ba2de926e493837a26cb60
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2026-06-04 11:03:26 +0300

    [Build] Remove common complier module from `fe10CompilerModules` list
    ^KT-86408

--- gradle/compilerModules.gradle.kts
+++ gradle/compilerModules.gradle.kts
@@ -102,7 +102,6 @@ val fe10CompilerModules = arrayOf(
     ":js:js.frontend",
     ":native:frontend.native",
     ":wasm:wasm.frontend",
-    ":compiler:backend.common.jvm",
 ).also { extra["fe10CompilerModules"] = it }
 
 /**
