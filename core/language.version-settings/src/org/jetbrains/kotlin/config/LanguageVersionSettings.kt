b1b8960aadeebcf8e0f83a3bb24d70fd68b11084
Grigorii.Solnyshkin <Grigorii.Solnyshkin@jetbrains.com>
2026-06-05 16:27:49 +0300

    ~~test~~

--- core/language.version-settings/src/org/jetbrains/kotlin/config/LanguageVersionSettings.kt
+++ core/language.version-settings/src/org/jetbrains/kotlin/config/LanguageVersionSettings.kt
@@ -666,7 +666,7 @@ enum class LanguageFeature(
     // K1 support only. We keep it, as it's currently unclear what to do with this feature in K2
     DisableCheckingChangedProgressionsResolve(sinceVersion = null, "KT-49276"),
 
-    CollectionLiterals(sinceVersion = null, issue = "KT-80489", enabledInLatestLVTests = true),
+    CollectionLiterals(sinceVersion = KOTLIN_2_4, issue = "KT-80489"),
     ProperFieldAccessGenerationForFieldAccessShadowedByKotlinProperty(sinceVersion = null, "KT-56386"),
     IrCrossModuleInlinerBeforeKlibSerialization(sinceVersion = null, sinceApiVersion = ApiVersion.KOTLIN_2_3, forcesPreReleaseBinaries = true, issue = "KT-79717"),
     UnnamedLocalVariables(sinceVersion = null, forcesPreReleaseBinaries = false, issue = "KT-74809"),
