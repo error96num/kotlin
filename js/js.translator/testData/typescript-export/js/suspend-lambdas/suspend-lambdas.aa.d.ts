8337cfdd8609951846fb6e844be85fa6a2a0e402
Artem Kobzar <artem.kobzar@jetbrains.com>
2026-06-06 13:42:07 +0200

    fixup! [K/JS] Allow exporting Kotlin suspend lambdas as JS async functions

--- js/js.translator/testData/typescript-export/js/suspend-lambdas/suspend-lambdas.aa.d.ts
+++ js/js.translator/testData/typescript-export/js/suspend-lambdas/suspend-lambdas.aa.d.ts
@@ -1,7 +1,6 @@
 declare namespace JS_TESTS {
     type Nullable<T> = T | null | undefined
     function KtSingleton<T>(): T & (abstract new() => any);
-
     namespace foo {
         const exportedSuspendLambda: () => Promise<string>;
         function produceSuspendLambda(): (p0: number) => Promise<number>;
